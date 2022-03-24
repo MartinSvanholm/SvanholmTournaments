2a2b9de8b9b4f26bbd8942edf55bd66573af63e6
MartinSvanholm <msvanh20@student.aau.dk>
2022-03-24 11:45:09 +0100

    Begun model configuration
    - added Server model

---
+++ Kode/SvanholmTournaments/SvanholmTournaments/Shared/Models/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvanholmTournaments.Shared.Models
+{
+    public class Server
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public int PlayersOnline { get; set; }
+
+        public string Ip { get; set; } = string.Empty;
+
+        public bool IsOn { get; set; }
+
+        public void StartServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
